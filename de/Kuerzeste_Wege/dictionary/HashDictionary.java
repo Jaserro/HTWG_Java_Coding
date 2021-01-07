1d5fa213e05c5e9d8e5a28032cde3a39caf6e69b
Jaserro <63459397+Jaserro@users.noreply.github.com>
2021-01-07 16:20:00 +0100

    added Kuerzeste_Wege

---
+++ de/Kuerzeste_Wege/dictionary/HashDictionary.java
@@ -0,0 +1,156 @@
+package dictionary;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.function.Predicate;
+
+public class HashDictionary<K, V> implements Dictionary<K, V> {
+
+    private LinkedList<Entry<K,V>>[] tab;
+    private static final int DEF_CAPACITY = 31;
+    private int size;
+
+    public HashDictionary(){
+        tab = new LinkedList[DEF_CAPACITY];
+        size = 0;
+    }
+
+    public HashDictionary(int i){
+        size = 0;
+        if(checkPrime(i)) {
+            tab = new LinkedList[i];
+        }else{
+            tab = new LinkedList[getNextPrime(i)];
+        }
+    }
+
+    @Override
+    public V insert(K key, V value) {
+        V oldV = search(key);
+        if (oldV != null){
+            int adr = searchAdr(key,tab);
+            for (Entry<K,V> aE: tab[adr]) {
+                if (key.equals(aE.getKey())){
+                    aE.setValue(value);
+                }
+            }
+        } else {
+            int adr = searchAdr(key,tab);
+
+            if (tab[adr] == null){
+                tab[adr] = new LinkedList<>();
+            }
+            tab[adr].add(new Entry<>(key,value));
+            size++;
+            if (tab[adr].size() > 2){
+                refactor();
+            }
+        }
+        return oldV;
+    }
+
+    @Override
+    public V search(K key) {
+
+        int adr = searchAdr(key,tab);
+
+        if (tab[adr] != null){
+
+            for (Entry<K,V> aE: tab[adr]) {
+                if (key.equals(aE.getKey())){
+                   return aE.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public int searchAdr(K key,LinkedList<Entry<K,V>>[] list){
+        int adr = key.hashCode();
+        if (adr < 0){
+            adr = -adr;
+        }
+        return adr % list.length;
+    }
+
+    @Override
+    public V remove(K key) {
+        int adr = searchAdr(key,tab);
+        V oldV = search(key);
+        for (int i = 0; i < tab[adr].size(); i++) {
+            if (oldV != null){
+                tab[adr].remove(i);
+                size--;
+            }
+        }
+        return oldV;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new Iterator<Entry<K, V>>() {
+            int currentIndex = 0;
+            int currentEntry = 0;
+            Iterator<Entry<K, V>> listIt = null;
+
+            @Override
+            public boolean hasNext() {
+                return currentEntry < size;
+            }
+
+            @Override
+            public Entry<K, V> next() {
+                assert hasNext();
+                if (listIt == null || !listIt.hasNext()) {
+                    while (tab[++currentIndex] == null || tab[currentIndex].size() == 0);
+                    listIt = tab[currentIndex].iterator();
+                }
+                currentEntry++;
+                return listIt.next();
+            }
+        };
+    }
+
+    private boolean checkPrime(int check){
+        for (int i = 2; i < check; i++) {
+            if (check % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getNextPrime(int prime){
+        prime++;
+        while (!checkPrime(prime)){
+            prime++;
+        }
+        return prime;
+    }
+
+
+
+    private void refactor(){
+        LinkedList<Entry<K,V>>[] temp = new LinkedList[getNextPrime(tab.length * 2)];
+
+        for (LinkedList<Entry<K,V>> ll: tab) {
+            if (ll == null){
+                continue;
+            }
+            for (Entry<K,V> aE: ll) {
+                int adr = searchAdr(aE.getKey(),temp);//fix Adresse der neune Liste errechnet
+                if (temp[adr] == null){
+                    temp[adr] = new LinkedList<>();
+                }
+                temp[adr].add(new Entry<>(aE.getKey(),aE.getValue()));
+            }
+        }
+        tab = temp;
+    }
+
+}
---
+++ de/Kuerzeste_Wege/shortestPath/ShortestPath.java
@@ -0,0 +1,174 @@
+package shortestPath;
+
+import directedGraph.*;
+import sim.SYSimulation;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+// ...
+
+/**
+ * Kürzeste Wege in Graphen mit A*- und Dijkstra-Verfahren.
+ * @author Oliver Bittel
+ * @since 27.01.2015
+ * @param <V> Knotentyp.
+ */
+public class ShortestPath<V> {
+	
+	SYSimulation sim = null;
+	
+	Map<V,Double> dist; // Distanz für jeden Knoten
+	Map<V,V> pred; // Vorgänger für jeden Knoten
+	DirectedGraph<V> dg;
+	Heuristic<V> dh;
+	V start;
+	V end;
+	LinkedList<V> path;
+	// ...
+
+	/**
+	 * Konstruiert ein Objekt, das im Graph g k&uuml;rzeste Wege 
+	 * nach dem A*-Verfahren berechnen kann.
+	 * Die Heuristik h schätzt die Kosten zwischen zwei Knoten ab.
+	 * Wird h = null gewählt, dann ist das Verfahren identisch 
+	 * mit dem Dijkstra-Verfahren.
+	 * @param g Gerichteter Graph
+	 * @param h Heuristik. Falls h == null, werden kürzeste Wege nach
+	 * dem Dijkstra-Verfahren gesucht.
+	 */
+	public ShortestPath(DirectedGraph<V> g, Heuristic<V> h) {
+		this.dg = g;
+		this.dh = h;
+		this.dist = new TreeMap<>();
+		this.pred = new TreeMap<>();
+		this.path = new LinkedList<>();
+	}
+
+	/**
+	 * Diese Methode sollte nur verwendet werden, 
+	 * wenn kürzeste Wege in Scotland-Yard-Plan gesucht werden.
+	 * Es ist dann ein Objekt für die Scotland-Yard-Simulation zu übergeben.
+	 * <p>
+	 * Ein typische Aufruf für ein SYSimulation-Objekt sim sieht wie folgt aus:
+	 * <p><blockquote><pre>
+	 *    if (sim != null)
+	 *       sim.visitStation((Integer) v, Color.blue);
+	 * </pre></blockquote>
+	 * @param sim SYSimulation-Objekt.
+	 */
+	public void setSimulator(SYSimulation sim) {
+		this.sim = sim;
+	}
+
+	/**
+	 * Sucht den kürzesten Weg von Starknoten s zum Zielknoten g.
+	 * <p>
+	 * Falls die Simulation mit setSimulator(sim) aktiviert wurde, wird der Knoten,
+	 * der als nächstes aus der Kandidatenliste besucht wird, animiert.
+	 * @param s Startknoten
+	 * @param g Zielknoten
+	 */
+	public void searchShortestPath(V s, V g) {
+		this.start = s;
+		this.end = g;
+		this.dist = new TreeMap<>();
+		this.pred = new TreeMap<>();
+		this.path = new LinkedList<>();
+
+		List<V> kl = new ArrayList<>();
+		for (var v : dg.getVertexSet() ) {
+			dist.put(v,Double.POSITIVE_INFINITY);
+			pred.put(v,null);
+		}
+		dist.put(s,0.0);
+		kl.add(s);
+
+		while(!kl.isEmpty()){
+
+			V min = null;
+			double distanz = Double.POSITIVE_INFINITY;
+
+			for (var v :kl ) {
+				if(dh != null){
+					if((dist.get(v) + dh.estimatedCost(v,g) ) < distanz){
+						min = v;
+						distanz = dist.get(v) + dh.estimatedCost(v,g);
+					}
+				}else {
+					if(dist.get(v) < distanz){
+						min = v;
+						distanz = dist.get(v);
+					}
+				}
+			}
+			printCurrentVertex(min);
+			if(g.equals(min)){
+				return;
+			}
+			kl.remove(min);
+
+			for (var w:	dg.getSuccessorVertexSet(min) ) {
+				if(dist.get(w) == Double.POSITIVE_INFINITY){
+					kl.add(w);
+				}
+				if(dist.get(min) + dg.getWeight(min,w) < dist.get(w)){
+					pred.put(w,min);
+					dist.put(w, dist.get(min) + dg.getWeight(min,w));
+				}
+			}
+		}
+	}
+
+	private void printCurrentVertex(V v){
+		if (sim != null)
+			sim.visitStation((Integer) v, Color.BLUE);
+	}
+
+
+	/**
+	 * Liefert einen kürzesten Weg von Startknoten s nach Zielknoten g.
+	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+	 * @return kürzester Weg als Liste von Knoten.
+	 */
+	public List<V> getShortestPath() {
+
+		LinkedList<V> path = new LinkedList<>();
+
+		path.add(end);
+
+		V current = pred.get(end);
+		if(current == null){
+			throw new IllegalArgumentException();
+		}
+		do
+		{
+			path.add(current);
+		} while ((current = pred.get(current)) != start);
+		path.add(start);
+
+		Collections.reverse(path);
+		return path;
+
+	}
+
+	public List<V> getShortestPathSteps() {
+		return this.path;
+	}
+
+	/**
+	 * Liefert die Länge eines kürzesten Weges von Startknoten s nach Zielknoten g zurück.
+	 * Setzt eine erfolgreiche Suche von searchShortestPath(s,g) voraus.
+	 * @throws IllegalArgumentException falls kein kürzester Weg berechnet wurde.
+	 * @return Länge eines kürzesten Weges.
+	 */
+	public double getDistance() {
+		Double current = dist.get(end);
+		if(current == null){
+			throw new IllegalArgumentException();
+		}
+		return this.dist.get(end);
+	}
+
+}
